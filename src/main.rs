c2b5d385cef84088fdd2a6192da37918da70d791
Jayanth Ramakkagari <jayantramakkagari@Jayanths-MacBook-Pro.local>
2026-01-06 16:31:00 -0500

    Lyrics problem

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    twelve_days_lyrics();
+}
+fn twelve_days_lyrics() {
+    let days: [&str; 12] = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eight", "ninth", "tenth", "eleventh", "twelfth"];
+    let gifts: [&str; 12] = ["A partridge in a pear tree", "Two turtle doves",
+     "Three French hens", "Four calling birds",
+     "Five golden rings", "Six geese a-laying", "Seven swans a-swimming",
+     "Eight maids a-milking", "Nine ladies dancing", "Ten lords a-leaping",
+     "Eleven pipers piping", "Twelve drummers drumming"];
+
+    for i in 0..days.len() {
+        println!("On the {} day of Christmas, My true love gave to me,", days[i]);
+
+        for j in (0..=i).rev() {
+            if j == 0 && i != 0 {
+                println!("And {}", gifts[0]);
+            } else {
+                println!("{}", gifts[j]);
+            }
+
+        }
+        println!();
+    }
+}
